e2451cd6d057b783f78de9d2ca8a97a58f141a88
David McClure <davidwilliammcclure@gmail.com>
2017-01-10 10:17:33 -0800

    Install Mobx.

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,40 @@
 
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Radium from 'radium';
+import {observable, action} from 'mobx';
+import {observer} from 'mobx-react';
+
+
+const state = observable({
+  timer: 0
+});
 
 
 @Radium
+@observer
 class Test extends Component {
 
   render() {
 
     const styles = {
-      color: 'red',
+      color: 'blue',
     };
 
-    return <h1 style={styles}>Test3</h1>;
+    return <h1 style={styles}>{this.props.state.timer}</h1>;
 
   }
 
 }
 
 
-ReactDOM.render(<Test />, document.getElementById('app'));
+ReactDOM.render(
+  <Test state={state} />,
+  document.getElementById('app')
+);
+
+
+setInterval(function() {
+  state.timer += 1;
+}, 1000);
